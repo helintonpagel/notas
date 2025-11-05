b823c56a540d44c1780a8020851d4baf860993c1
Hélinton <helintonmello@gmail.com>
2025-11-05 18:43:34 -0300

    Corrige a navegação para usar window.location.href ao invés de window.location.replace nas funções de edição e exclusão de anotações. (Problema de cache)

--- js/view.js
+++ js/view.js
@@ -21,21 +21,21 @@ noteForm.addEventListener("submit", (event) => {
   const notes = JSON.parse(localStorage.getItem("notes")) || [];
   const noteIndex = notes.findIndex((note) => note.id === new URLSearchParams(window.location.search).get("id"));
 
-  if (noteIndex === -1) alert("Anotação não encontrada!") || window.location.replace("index.html");
+  if (noteIndex === -1) alert("Anotação não encontrada!") || window.location.href("index.html");
 
   notes[noteIndex].title = noteForm.elements["note-title"].value;
   notes[noteIndex].content = noteForm.elements["note-content"].value;
   notes[noteIndex].updatedAt = new Date().toISOString();
 
   localStorage.setItem("notes", JSON.stringify(notes));
-  window.location.replace("index.html");
+  window.location.href("index.html");
 });
 
 deleteButton.addEventListener("click", () => {
   const notes = JSON.parse(localStorage.getItem("notes")) || [];
   const noteIndex = notes.findIndex((note) => note.id === new URLSearchParams(window.location.search).get("id"));
 
-  if (noteIndex === -1) alert("Anotação não encontrada!") || window.location.replace("index.html");
+  if (noteIndex === -1) alert("Anotação não encontrada!") || window.location.href("index.html");
   if (!confirm("Tem certeza que deseja deletar esta anotação?")) return;
 
   notes.splice(noteIndex, 1);
